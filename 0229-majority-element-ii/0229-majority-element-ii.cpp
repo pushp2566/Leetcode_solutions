ceecbadd35947f305ebeabcd6f1cb3be5d638fc7
pushp2566 <145247050+pushp2566@users.noreply.github.com>
2026-08-20 19:58:29 +0530

    Time: 0 ms (100%), Space: 23.7 MB (79.33%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,53 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
-        int c1 = 0, c2 = 0;
-        int n1 = 0, n2 = 0;
-
-        // Find potential candidates
-        for(int x : nums) {
-
-            if(x == c1) {
-                n1++;
-            }
-            else if(x == c2) {
-                n2++;
-            }
-            else if(n1 == 0) {
-                c1 = x;
-                n1 = 1;
-            }
-            else if(n2 == 0) {
-                c2 = x;
-                n2 = 1;
-            }
-            else {
-                n1--;
-                n2--;
-            }
+        int n=nums.size();
+        int opt1=0,opt2=0,ct1=0,ct2=0;
+     for(int i=0;i<n;i++){
+        if(nums[i]==opt1){
+            ct1++;
+        }
+        else if(nums[i]==opt2){
+            ct2++;
+        }
+        else if(ct1==0){
+            ct1=1;opt1=nums[i];
+        }
+        else if(ct2==0){
+            ct2=1;opt2=nums[i];
+        }
+        else{
+            ct1--;ct2--;
         }
 
-        // Verify candidates
-        n1 = 0;
-        n2 = 0;
+     }
 
-        for(int x : nums) {
-            if(x == c1)
-                n1++;
+     ct1=0,ct2=0;
+     for(int i=0;i<n;i++){
+if(nums[i]==opt1)ct1++;
+else if(nums[i]==opt2)ct2++;
+     }
 
-            else if(x == c2)
-                n2++;
-        }
+vector<int>ans;
+if(ct1>(n/3))ans.push_back(opt1);
+if(ct2>(n/3))ans.push_back(opt2);
 
-        vector<int> ans;
 
-        if(n1 > nums.size() / 3)
-            ans.push_back(c1);
+return ans;
 
-        if(n2 > nums.size() / 3)
-            ans.push_back(c2);
-
-        return ans;
     }
 };
