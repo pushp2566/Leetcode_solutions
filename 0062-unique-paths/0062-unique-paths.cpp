c065264312c633b3742025ee482616fc9562bee0
pushp2566 <145247050+pushp2566@users.noreply.github.com>
2026-08-21 21:50:39 +0530

    Time: 3 ms (9.4%), Space: 9.5 MB (5.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -8,8 +8,8 @@ public:
          if(dp[i][j]!=-1)return dp[i][j];
          return dp[i][j]=solve(i+1,j,dp)+solve(i,j+1,dp);
     }
-    int uniquePaths(int m, int n) {
-        swap(n,m);
+    int uniquePaths(int n, int m) {
+       // swap(n,m);
         vector<vector<int>>dp(n,vector<int>(m,-1));
        return solve(0,0,dp);
 
