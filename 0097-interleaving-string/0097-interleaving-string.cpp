46ee1bb47c1eddfae16f8c66c7e5f828bcd7fe14
pushp2566 <145247050+pushp2566@users.noreply.github.com>
2026-09-06 00:54:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (32.84%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool dfs(int i,int j, string & s1,string & s2,string & s3,vector<vector<int>>&dp ){
+        int n=s1.size();
+        int m=s2.size();
+        if(i==n&&j==m&&i+j==s3.size())return true;
+        if(i==n&&j==m)return false;
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        int k=i+j;
+
+        if(i==n){
+            if(s3[k]==s2[j]){
+             dp[i][j]=dfs(i,j+1,s1,s2,s3,dp);
+            }
+            else{
+                return dp[i][j]=false;
+            }
+        }
+
+        
+        if(j==m){
+            if(s3[k]==s1[i]){
+             dp[i][j]=dfs(i+1,j,s1,s2,s3,dp);
+            }
+            else{
+                return dp[i][j]=false;
+            }
+        }
+
+
+        bool res=false;
+        if(s3[k]==s2[j]){
+             res=dfs(i,j+1,s1,s2,s3,dp);
+            }
+
+        if(s3[k]==s1[i]){
+        res=(res|dfs(i+1,j,s1,s2,s3,dp));
+        }
+
+
+        
+        return dp[i][j]=res;
+
+
+
+
+
+
+
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        int n=s1.size();
+        int m=s2.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return dfs(0,0,s1,s2,s3,dp);
+        
+
+
+        
+    }
+};
