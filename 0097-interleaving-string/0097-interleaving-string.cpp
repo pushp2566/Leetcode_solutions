ee3293a89c55189d03270e5fd3f9ad1c2d1a3234
pushp2566 <145247050+pushp2566@users.noreply.github.com>
2026-09-06 01:10:27 +0530

    Time: 6 ms (25.9%), Space: 9.8 MB (39.98%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -8,7 +8,6 @@ public:
         if(dp[i][j]!=-1)return dp[i][j];
 
         int k=i+j;
-
         if(i==n){
             if(s3[k]==s2[j]){
              dp[i][j]=dfs(i,j+1,s1,s2,s3,dp);
@@ -16,9 +15,7 @@ public:
             else{
                 return dp[i][j]=0;
             }
-        }
-
-        
+        }   
         if(j==m){
             if(s3[k]==s1[i]){
              dp[i][j]=dfs(i+1,j,s1,s2,s3,dp);
@@ -27,8 +24,6 @@ public:
                 return dp[i][j]=0;
             }
         }
-
-
         bool res=false;
         if(s3[k]==s2[j]){
              res=dfs(i,j+1,s1,s2,s3,dp);
@@ -37,27 +32,59 @@ public:
         if(s3[k]==s1[i]){
         res=(res|dfs(i+1,j,s1,s2,s3,dp));
         }
-
-
         
         return dp[i][j]=res;
-
-
-
-
-
-
 
     }
     bool isInterleave(string s1, string s2, string s3) {
         int n=s1.size();
         int m=s2.size();
         
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return dfs(0,0,s1,s2,s3,dp)==1;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+       
+        dp[n][m]=1;
+        for(int i=n;i>=0;i--){
+            for(int j=m;j>=0;j--){
+             //
+                 if(i==n&&j==m&&i+j==s3.size())continue;
+                 if(i==n&&j==m)return false;
+  
+       // if(dp[i][j]!=-1)return dp[i][j];
+
+        int k=i+j;
+        if(i==n){
+            if(s3[k]==s2[j]){
+             dp[i][j]=dp[i][j+1];
+            }
+            else{
+                 dp[i][j]=0;
+            }
+        }   
+        if(j==m){
+            if(s3[k]==s1[i]){
+             dp[i][j]=dp[i+1][j];
+            }
+            else{
+                 dp[i][j]=0;
+            }
+        }
+        bool res=false;
+        if(s3[k]==s2[j]){
+             res=dp[i][j+1];
+            }
+
+        if(s3[k]==s1[i]){
+        res=(res|dp[i+1][j]);
+        }
+        
+         dp[i][j]=res;
+             //
+            }
+        }
         
 
-
+//  return dfs(0,0,s1,s2,s3,dp)==1;
+ return dp[0][0]==1;
         
     }
 };
