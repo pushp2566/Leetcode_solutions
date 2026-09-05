4758fe41d62fbcab349576ed012a5cbb993b1657
pushp2566 <145247050+pushp2566@users.noreply.github.com>
2026-09-06 03:05:11 +0530

    Time: 13 ms (48.19%), Space: 18.5 MB (36.89%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-    bool solve(string &s,int i, int j,vector<vector<int>>&dp){
-         int n=s.size();
-            if(i<0||j>=n)return false;
-            if(i==j)dp[i][j]=1;
-            if(j==i+1&&s[i]==s[j])dp[i][j]=1;
-            if(dp[i][j]!=-1)return dp[i][j]==1;
-            if(s[i]!=s[j]){dp[i][j]=0;
-                return false;
-            
-            }
-            return dp[i][j]=solve (s,i+1,j-1,dp);
 
+    string expand(string &s, int l, int r) {
 
-
-    }
-
-
-    string longestPalindrome(string s) {
-        int n=s.size();
-        int maxi=0;
-        string ans="";
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                   if(solve(s,i,j,dp)){
-                        if(j-i+1>maxi){
-                            maxi =j-i+1;
-                            ans=s.substr(i,j-i+1);
-                        }
-                   }
-            }
+        while(l >= 0 && r < s.size() && s[l] == s[r]) {
+            l--;
+            r++;
         }
 
-       
-                
+        return s.substr(l + 1, r - l - 1);
+    }
 
- 
+    string longestPalindrome(string s) {
 
-      return ans;
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++) {
+
+            // Odd length palindrome
+            string temp1 = expand(s, i, i);
+
+            // Even length palindrome
+            string temp2 = expand(s, i, i + 1);
+
+            if(temp1.size() > ans.size())
+                ans = temp1;
+
+            if(temp2.size() > ans.size())
+                ans = temp2;
+        }
+
+        return ans;
     }
 };
